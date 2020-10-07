1134b7f57204b760f11391d391ed13e18bb18513
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-06 19:57:42 -0500

    setup basic search bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Location..."/>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      apiKey: process.env.VUE_APP_KEY
+    }
+  }
 }
 </script>
 
